3d3fa865551c11225aa5eb55be31da1713e43091
mash-pg <ymanshou@gmail.com>
2025-04-06 13:56:17 +0900

    アルゴリズムとデータ構造①

---
+++ algorithm/アルゴリズムとデータ構造/code2-2-1.cpp
@@ -0,0 +1,13 @@
+#include "/home/mash/projects/algorithm/include/template.hpp"
+/*
+O(N) for文1回
+*/
+int main(){
+    int N;
+    cin >> N;
+    
+    int count = 0;
+    for(int i = 0; i < N; ++i){
+        ++count;
+    }
+}
---
+++ algorithm/アルゴリズムとデータ構造/code2-2-2.cpp
@@ -0,0 +1,15 @@
+#include "/home/mash/projects/algorithm/include/template.hpp"
+/*
+O(N^2) for文2回
+*/
+int main(){
+    int N;
+    cin >> N;
+    
+    int count = 0;
+    for(int i = 0; i < N; ++i){
+        for(int i = 0; i < N;++i){
+        ++count;
+        }
+    }
+}
