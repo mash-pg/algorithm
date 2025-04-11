3fb168465ff602d8b65f1727eb00b6502dc9d256
mash-pg <ymanshou@gmail.com>
2025-04-11 15:31:20 +0900

    バブルソートの実装とソース理解

---
+++ algorithm/アルゴリズム基礎/saiki.cpp
@@ -0,0 +1,19 @@
+#include "/home/mash/projects/algorithm/include/template.hpp"
+
+int main(){
+    //ダミーを作成する必要があるため、0を追加
+    //1～6のため下記データになる。
+    vl list = {0,21,5,53,71,3,17};
+    ll n = list.size() -1;
+    ll cnt = 0;
+    for(ll i = 1; i <= n-1; i++){
+        for(ll j = n; j >= i+1; j--){
+            if(list[j - 1] > list[j]){
+                swap(list[j - 1],list[j]);
+                cnt++;
+            }
+        }
+    }
+    cout << "count : " << cnt << endl;
+    for(ll i = 0; i < n; i++) cout << " " << list[i]; 
+}
