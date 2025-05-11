154672e998ac83223bcc863ed8be874d81a8d1dd
mash-pg <ymanshou@gmail.com>
2025-05-11 18:53:59 +0900

    セグメント木と動的計画法

---
+++ algorithm/アルゴリズム基礎/動的計画法/dpknapsack.cpp
@@ -0,0 +1,90 @@
+#ifndef MY_TEMPLATE_HPP
+#define MY_TEMPLATE_HPP
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>  // これをインクルード！
+#include <cmath>
+// 多倍長整数
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG // デバッグ用（オンラインジャッジでは無効）
+#endif
+
+// マクロ定義（セミコロン削除・安全な形式）
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES do { cout << "Yes" << endl; } while (0)
+#define NO do { cout << "No" << endl; } while (0)
+#define YN { cout << "Yes" << endl; } else { cout << "No" << endl; }
+#define dame do { cout << -1 << endl; } while (0)
+//改行処理
+#define ce cout << endl;
+//Map追加
+#define myM std::unordered_map<std::string, int> myMap
+// 名前空間
+namespace multip = boost::multiprecision;
+using multip::cpp_int;
+
+using namespace atcoder;
+using namespace std;
+
+// 型エイリアス
+using ll = long long;
+using lll = multip::int128_t;
+using ld = long double;
+using P = pair<ll, ll>;
+using mint = modint1000000007;
+
+// テンプレート型エイリアス
+template<class T> using vc = vector<T>;
+template<class T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;
+
+// 配列のエイリアス
+using vl = vc<ll>;
+using vvl = vv<ll>;
+using vvvl = vv<vl>;
+using vvvvl = vv<vvl>;
+
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vv<vi>;
+using vvvvi = vv<vvi>;
+
+using vs = vc<string>;
+using vvs = vv<string>;
+using vvvs = vv<vs>;
+
+using vchar = vc<char>;
+using vvchar = vv<char>;
+using vvvchar = vv<vchar>;
+
+using vld = vc<ld>;
+using vvld = vv<ld>;
+using vvvld = vv<vld>;
+
+using vmint = vc<mint>;
+using vvmint = vv<mint>;
+using vvvmint = vv<vmint>;
+
+// for 文の短縮
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define per(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
+// C++20 以降で使えるラムダ関数版 rep
+auto rep = [](int n, auto f) { for (int i = 0; i < n; i++) f(i); };
+#endif // MY_TEMPLATE_HPP
+#define arrn(arr) (sizeof(arr) / sizeof(arr[0]))
+
+
+int main(){
+    int n,w;
+    cin >> n >> w;
+}
---
+++ algorithm/アルゴリズム基礎/動的計画法/dpstairs.cpp
@@ -0,0 +1,106 @@
+#ifndef MY_TEMPLATE_HPP
+#define MY_TEMPLATE_HPP
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>  // これをインクルード！
+#include <cmath>
+// 多倍長整数
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG // デバッグ用（オンラインジャッジでは無効）
+#endif
+
+// マクロ定義（セミコロン削除・安全な形式）
+#define pb push_back
+#define eb emplace_back
+#define em emplace
+#define pob pop_back
+#define YES do { cout << "Yes" << endl; } while (0)
+#define NO do { cout << "No" << endl; } while (0)
+#define YN { cout << "Yes" << endl; } else { cout << "No" << endl; }
+#define dame do { cout << -1 << endl; } while (0)
+//改行処理
+#define ce cout << endl;
+//Map追加
+#define myM std::unordered_map<std::string, int> myMap
+// 名前空間
+namespace multip = boost::multiprecision;
+using multip::cpp_int;
+
+using namespace atcoder;
+using namespace std;
+
+// 型エイリアス
+using ll = long long;
+using lll = multip::int128_t;
+using ld = long double;
+using P = pair<ll, ll>;
+using mint = modint1000000007;
+
+// テンプレート型エイリアス
+template<class T> using vc = vector<T>;
+template<class T> using vv = vc<vc<T>>;
+template<class T> using pq = priority_queue<T, vc<T>>;
+template<class T> using pq_g = priority_queue<T, vc<T>, greater<T>>;
+
+// 配列のエイリアス
+using vl = vc<ll>;
+using vvl = vv<ll>;
+using vvvl = vv<vl>;
+using vvvvl = vv<vvl>;
+
+using vi = vc<int>;
+using vvi = vv<int>;
+using vvvi = vv<vi>;
+using vvvvi = vv<vvi>;
+
+using vs = vc<string>;
+using vvs = vv<string>;
+using vvvs = vv<vs>;
+
+using vchar = vc<char>;
+using vvchar = vv<char>;
+using vvvchar = vv<vchar>;
+
+using vld = vc<ld>;
+using vvld = vv<ld>;
+using vvvld = vv<vld>;
+
+using vmint = vc<mint>;
+using vvmint = vv<mint>;
+using vvvmint = vv<vmint>;
+
+// for 文の短縮
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define per(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
+// C++20 以降で使えるラムダ関数版 rep
+auto rep = [](int n, auto f) { for (int i = 0; i < n; i++) f(i); };
+#endif // MY_TEMPLATE_HPP
+#define arrn(arr) (sizeof(arr) / sizeof(arr[0]))
+
+// 💬 問題文：
+// ある人が n 段の階段を上ろうとしています。
+// 1回の行動で「1段」または「2段」登ることができます。
+// この人が n 段目に到達する方法は何通りあるでしょうか？
+// 答えを mod 1000000007 で出力してください。
+const int MOD = 1000000007;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<long long> dp(n+1);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+    }
+
+    cout << dp[n] << endl;
+    return 0;
+}
