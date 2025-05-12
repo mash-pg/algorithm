af431a5ec013a1df46cad0dedc76d64e70f91a0e
mash-pg <ymanshou@gmail.com>
2025-05-12 23:59:53 +0900

    DP経路の履歴処理

--- algorithm/アルゴリズム基礎/動的計画法/dpgridblock.cpp
+++ algorithm/アルゴリズム基礎/動的計画法/dpgridblock.cpp
@@ -75,58 +75,76 @@ using vmint = vc<mint>;
 using vvmint = vv<mint>;
 using vvvmint = vv<vmint>;
 
-// for 文の短縮
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+// or 文の短縮
+#define rep(j, n) for (int i = 0; i < (int)(n); i++)
 #define per(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
 // C++20 以降で使えるラムダ関数版 rep
 auto rep = [](int n, auto f) { for (int i = 0; i < n; i++) f(i); };
 #endif // MY_TEMPLATE_HPP
 #define arrn(arr) (sizeof(arr) / sizeof(arr[0]))
 
-// 💬 問題文：
-// H×W のマス目があり、左上 (0,0) から右下 (H-1,W-1) に移動します。
-// 1回で 右 または 下 に1マス進めます。
-// ただし、一部のマスには障害物（'#'）があって通れません。
-// 右下まで行く 通り数 を mod 1000000007 で求めてください。
-
-
 const int MOD = 1000000007;
-
-int main() {
+int main(){
+    /*
+    W:Width
+    H:High
+    R:Rihgt
+    D:Down
+    
+    */
     int H,W;
     cin >> H >> W;
-    vvs grid(H,vs(W));
-    //配列にデータを格納
-    rep(i,H){
-        string row;
-        cin >>row;
-        rep(j,W) {
-            grid[i][j] = row[j];
-        }
-    }
-    //dp初期設定
+    vector<string> grid(H);
+    for(int i = 0; i< H;i++) cin >> grid[i];
+    
     vvl dp(H,vl(W,0));
+    vvchar from(H,vchar(W,'-'));
 
-    //スタート地点
-    if(grid[0][0] == ".") dp[0][0] = 1;
+    if(grid[0][0] == '.') dp[0][0] = 1;
 
-    //縦と横のデータを処理していく
-    for(int i = 0; i< H;i++){
-        for(int j = 0; j < W;j++){
-            //行き止まりがあったらスキップ
-            if (grid[i][j] == "#") continue;
-            //iが0だと
-            if(i > 0){
-                dp[i][j] += dp[i - 1][j];
-                dp[i][j] %= MOD;  
+    for(int i = 0; i < H;i++){
+        for (int j = 0; j < W; j++)
+        {
+            if(grid[i][j] == '#') continue;
+            if(i > 0 && grid[i - 1][j] == '.' && dp[i - 1][j] > 0) {
+                //列の上から下の順にdp配列に格納する
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+                from[i][j] = 'D';
             }
-            if(j > 0){
-                dp[i][j] += dp[i][j - 1];
-                dp[i][j] %= MOD; 
+            if(j > 0 && grid[i][j - 1] == '.' && dp[i][j - 1] > 0) {
+                //行の左から右の順にdp配列に格納する
+                dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
+                //整合性を保つために辞書順にR優先にしている
+                if (from[i][j] == '-' || from[i][j] > 'R')
+                    from[i][j] = 'R';
             }
         }
+        
     }
-    //ゴールの地点を見つける
-    cout << dp[H - 1][W - 1] << endl;
+
+    //復元フェーズ
+    string path;
+    int i = H - 1;
+    int j = W -1;
+    if(dp[i][j] == 0){
+        cout << "No path\n";
+        return 0;
+    }
+
+    while (i != 0 || j != 0) {
+        if (from[i][j] == 'D') {
+            path += 'D';
+            i--;
+        } else if (from[i][j] == 'R') {
+            path += 'R';
+            j--;
+        } else {
+            break;
+        }
+    }
+    reverse(path.begin(), path.end());
+    cout << "number of paths："<< dp[H-1][W-1] << endl;
+    cout << "Path：" << path << endl;
     return 0;
+
 }
