c0cc8c8214259d726143ecb4d3d4fa63cb37b088
mash-pg <ymanshou@gmail.com>
2025-07-27 13:05:34 +0900

    bfsのプログラム作成

---
+++ algorithm/antbook/bfs.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+const int INF = 1000000;
+const int MAX_N = 100;
+const int MAX_M = 100;
+
+char maze[MAX_N][MAX_M + 1];
+int N,M;
+int sx,sy;
+int gx,gy;
+int d[MAX_N][MAX_M];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+typedef pair<int , int> P;
+
+int bfs(){
+    queue<P> que;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) { // 列数Mでループ
+            d[i][j] = INF;
+        }
+    }
+    que.push(P(sx,sy));
+    d[sx][sy] = 0;
+
+    while(que.size()){
+        P p = que.front();
+        que.pop();
+
+        if(p.first == gx && p.second == gy) break;
+        for(int i = 0; i < 4;i++){
+            int nx = p.first + dx[i];
+            int ny = p.second + dy[i];
+
+            if(0 <= nx && nx < N && 0 <= ny && ny < M && maze[nx][ny] != '#' && d[nx][ny] == INF){
+                que.push(P(nx,ny));
+                d[nx][ny] = d[p.first][p.second] + 1;
+            }
+        }
+    }
+    return d[gx][gy];
+}
+
+int main(){
+
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> maze[i];
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (maze[i][j] == 'S') {
+                sx = i;
+                sy = j;
+            } else if (maze[i][j] == 'G') {
+                gx = i;
+                gy = j;
+            }
+        }
+    }
+    int res = bfs();
+    cout << res << endl;
+
+    return 0;
+}
