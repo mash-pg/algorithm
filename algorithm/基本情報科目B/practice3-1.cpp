60a120e98c922052abf3f057b23f0796a16c7669
mash-pg <ymanshou@gmail.com>
2025-08-26 22:43:24 +0900

    面白い条件式日本語って難しいね（笑）

---
+++ algorithm/基本情報科目B/practice3-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int num;
+    int val;
+    num = 1;
+    val = 100;
+    // while(num >= 100){
+    //     cout << num << endl;
+    //     num = num + 1;
+    // }
+    while(num < 100){
+        cout << num << endl;
+        num = num + 1;
+    }
+    cout << endl;
+    // while(0 <= val && val >= 100){
+    //     cout << val << endl;
+    //     val = val - 1;
+    // }
+    while(val >= 1){
+        cout << val << endl;
+        val = val - 1;
+    }
+
+}
